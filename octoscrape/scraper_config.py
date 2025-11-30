882cdd2480ffc2846e9181a0477344eb3255094c
d-laslo <ice_bear@ukr.net>
2025-11-30 05:31:48 +0200

    added config

---
+++ octoscrape/scraper_config.py
@@ -0,0 +1,18 @@
+import yaml
+from .config import (
+    CommonConfig, 
+    ScraperConfig, 
+    MultiScraperConfig
+)
+
+
+with open("config.yaml", "r", encoding="utf-8") as f:
+    raw_config = yaml.safe_load(f)
+
+common_config = CommonConfig(raw_config["shared"])
+
+scrappers_configs: dict[str, ScraperConfig |  MultiScraperConfig] = {
+    key : MultiScraperConfig(value, key) if value.get("is_multiscraper", False) 
+    else ScraperConfig(value, key)
+    for key, value in raw_config["process"].items()
+}
